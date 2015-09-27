071b46c21c3a469d03be46f905ec8de8d3872571
Florian Larysch <fl@n621.de>
2015-09-27 16:25:39 +0200

    fixes

--- bbb/buildroot/board/vaporsound/post-image.sh
+++ bbb/buildroot/board/vaporsound/post-image.sh
@@ -3,4 +3,4 @@
 # 2014, Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
 
 # copy the uEnv.txt to the output/images directory
-cp board/beaglebone/uEnv.txt $BINARIES_DIR/uEnv.txt
+cp ${BR2_EXTERNAL}/board/vaporsound/uEnv.txt $BINARIES_DIR/uEnv.txt
