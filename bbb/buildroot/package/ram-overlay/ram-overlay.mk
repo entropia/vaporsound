84d19cfbd0e2c93811eb48f52c19cf3278c24c81
Florian Larysch <fl@n621.de>
2016-04-16 08:53:46 -0400

    fix ram-overlay build def

--- bbb/buildroot/package/ram-overlay/ram-overlay.mk
+++ bbb/buildroot/package/ram-overlay/ram-overlay.mk
@@ -7,11 +7,11 @@ RAM_OVERLAY_LICENSE = GPLv2
 
 define RAM_OVERLAY_BUILD_CMDS
 	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
-		package/ram-overlay/mount_overlay.c -o $(@D)/mount_ram_overlay
+		$(BR2_EXTERNAL)/package/ram-overlay/mount_overlay.c -o $(@D)/mount_ram_overlay
 endef
 
 define RAM_OVERLAY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/mount)_ram_overlay $(TARGET_DIR)/usr/sbin/mount_ram_overlay
+	$(INSTALL) -D -m 755 $(@D)/mount_ram_overlay $(TARGET_DIR)/usr/sbin/mount_ram_overlay
 endef
 
 $(eval $(generic-package))
