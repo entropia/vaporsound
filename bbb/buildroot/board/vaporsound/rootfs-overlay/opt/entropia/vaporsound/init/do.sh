ba33b22b7dbad2962bce5594f2d2fe948cea2c35
Florian Larysch <fl@n621.de>
2016-04-16 23:46:53 +0200

    init: abort on failure

--- bbb/buildroot/board/vaporsound/rootfs-overlay/opt/entropia/vaporsound/init/do.sh
+++ bbb/buildroot/board/vaporsound/rootfs-overlay/opt/entropia/vaporsound/init/do.sh
@@ -3,4 +3,13 @@
 for i in /opt/entropia/vaporsound/init/init.d/*
 do
 	$i
+
+	if [ "$?" -ne 0 ]
+	then
+		NAME=$(basename "$i")
+
+		echo "init step $NAME failed, dropping to shell"
+
+		/bin/sh
+	fi
 done
